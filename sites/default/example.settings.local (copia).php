1a3a7172b42dda1639109610c32d435f6b63af93
Priscilla <you@example.com>
2017-03-31 10:13:57 -0600

    initial commit

---
+++ sites/default/example.settings.local (copia).php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Local settings file.
+ */
+
+$databases['default']['default'] = array(
+  'driver' => 'mysql',
+  'database' => 'drupaldb2',
+  'username' => 'drupaluser',
+  'password' => 'drupalpassword',
+  'host' => 'localhost',
+  'prefix' => 'main_',
+  'collation' => 'utf8_general_ci',
+);
